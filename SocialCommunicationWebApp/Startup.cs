c54ab0b81a550679b7de119f9939e55ad2360a6b
Razaul Munir <razaulmunir@gmail.com>
2019-10-20 08:15:05 +0600

    Project Setup
    just create a project

---
+++ SocialCommunicationWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialCommunicationWebApp.Startup))]
+namespace SocialCommunicationWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
