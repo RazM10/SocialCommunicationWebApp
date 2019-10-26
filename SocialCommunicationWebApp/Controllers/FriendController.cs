8857c6844ac99c510215832942a243835193377d
Razaul Munir <razaulmunir@gmail.com>
2019-10-27 00:32:31 +0600

    Add Friend partially done
    -not done with session(due)
    -found all property value of friend mode

---
+++ SocialCommunicationWebApp/Controllers/FriendController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SocialCommunicationWebApp.Models;
+
+namespace SocialCommunicationWebApp.Controllers
+{
+    public class FriendController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public FriendController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Friend
+        public ActionResult Index()
+        {
+            List<User> users =_context.UsercSet.ToList();
+            return View(users);
+        }
+
+        public ActionResult FriendRequest(int id)
+        {
+            Friend friend=new Friend();
+            string email = (string) Session["email"];
+            User user = _context.UsercSet.SingleOrDefault(u => u.Email == email);
+
+            if (user != null)
+            {
+                friend.UserFromId = user.Id;
+                friend.UserToId = id;
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SocialCommunicationWebApp/ViewModels/UserFriend_ViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialCommunicationWebApp.ViewModels
+{
+    public class UserFriend_ViewModel
+    {
+    }
+}
